67f8d5732c4add5ff93d0e62b576f95c11c9b234
Magnus Dale <md@sekal.com>
2022-06-20 12:36:35 +0200

    change text2

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Super cool stuff from feature branch <code>src/App.js</code> and save to reload.
+          Super cool stuff from feature branch more<code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
